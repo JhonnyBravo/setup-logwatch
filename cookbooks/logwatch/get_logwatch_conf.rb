01f8345c48fe0768d3e18cd1f8b1b80300041a2b
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-04-13 01:04:16 +0900

    new file: cookbooks/logwatch/get_logwatch_conf.rb

---
+++ cookbooks/logwatch/get_logwatch_conf.rb
@@ -0,0 +1,32 @@
+# get_logwatch_conf.rb
+# {
+#   /etc/logwatch/conf/logwatch.conf,
+#   /usr/share/logwatch/default.conf/logwatch.conf
+# } -> ~/Templates/logwatch/logwatch.conf.erb
+
+require 'dotenv'
+Dotenv.load
+
+USER_NAME=ENV["USER_NAME"]
+SRC1="/etc/logwatch/conf/logwatch.conf"
+SRC2="/usr/share/logwatch/default.conf/logwatch.conf"
+DST="/home/#{USER_NAME}/Templates/logwatch/"
+
+directory DST do
+  action :create
+  user USER_NAME
+  owner USER_NAME
+  group USER_NAME
+end
+
+execute "get_logwatch_conf_from_usr" do
+  not_if "test -f #{SRC1}"
+  user USER_NAME
+  command "install -m 644 #{SRC2} #{DST}"
+end
+
+execute "get_logwatch_conf_from_etc" do
+  only_if "test -f #{SRC1}"
+  user USER_NAME
+  command "install -m 644 #{SRC1} #{DST}"
+end
