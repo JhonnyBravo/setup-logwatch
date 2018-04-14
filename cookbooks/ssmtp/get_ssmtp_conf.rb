877782d7167fd20bf65e5d32bd9fbbe1944263d3
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-04-15 02:38:42 +0900

    new file: cookbooks/ssmtp/get_ssmtp_conf.rb

---
+++ cookbooks/ssmtp/get_ssmtp_conf.rb
@@ -0,0 +1,25 @@
+# get_ssmtp_conf.rb
+# /etc/ssmtp/ssmtp.conf -> ~/Templates/ssmtp/ssmtp.conf.erb
+
+require 'dotenv'
+Dotenv.load
+
+USER_NAME=ENV["USER_NAME"]
+SRC="/etc/ssmtp/ssmtp.conf"
+DST="/home/#{USER_NAME}/Templates/ssmtp/"
+
+directory DST do
+  action :create
+  user USER_NAME
+  owner USER_NAME
+  group USER_NAME
+end
+
+template "#{DST}ssmtp.conf" do
+  action :create
+  user USER_NAME
+  owner USER_NAME
+  group USER_NAME
+  mode "644"
+  source SRC
+end
