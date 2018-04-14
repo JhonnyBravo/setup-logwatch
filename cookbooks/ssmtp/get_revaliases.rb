76939523ad3ae364304e8644db5e4acdc6582c7f
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-04-15 02:38:13 +0900

    new file: cookbooks/ssmtp/get_revaliases.rb

---
+++ cookbooks/ssmtp/get_revaliases.rb
@@ -0,0 +1,25 @@
+# get_revaliases.rb
+# /etc/ssmtp/revaliases -> ~/Templates/ssmtp/revaliases.erb
+
+require 'dotenv'
+Dotenv.load
+
+USER_NAME=ENV["USER_NAME"]
+SRC="/etc/ssmtp/revaliases"
+DST="/home/#{USER_NAME}/Templates/ssmtp/"
+
+directory DST do
+  action :create
+  user USER_NAME
+  owner USER_NAME
+  group USER_NAME
+end
+
+template "#{DST}revaliases" do
+  action :create
+  user USER_NAME
+  owner USER_NAME
+  group USER_NAME
+  mode "644"
+  source SRC
+end
