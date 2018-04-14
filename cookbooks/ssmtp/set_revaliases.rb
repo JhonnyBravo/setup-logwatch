c35b4aa66f546946ebefa15bf6dddd7d0c34c219
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-04-15 02:40:09 +0900

    new file: cookbooks/ssmtp/set_revaliases.rb

---
+++ cookbooks/ssmtp/set_revaliases.rb
@@ -0,0 +1,15 @@
+# set_revaliases.rb
+# ~/Templates/ssmtp/revaliases.erb ->  /etc/ssmtp/revaliases
+
+require 'dotenv'
+Dotenv.load
+
+USER_NAME=ENV["USER_NAME"]
+SRC="/home/#{USER_NAME}/Templates/ssmtp/revaliases"
+DST="/etc/ssmtp/revaliases"
+
+template DST do
+  action :create
+  mode "644"
+  source SRC
+end
