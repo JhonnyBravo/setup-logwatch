3fed636fbbd99921f79c35bc62e1107e2c7ba5e5
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-04-17 01:01:37 +0900

    modified: cookbooks/logwatch/get_logwatch_conf.rb
    テンプレートファイルのコピー先ファイル名を修正。

--- cookbooks/logwatch/get_logwatch_conf.rb
+++ cookbooks/logwatch/get_logwatch_conf.rb
@@ -22,11 +22,11 @@ end
 execute "get_logwatch_conf_from_usr" do
   not_if "test -f #{SRC1}"
   user USER_NAME
-  command "install -m 644 #{SRC2} #{DST}"
+  command "install -m 644 #{SRC2} #{DST}logwatch.conf.erb"
 end
 
 execute "get_logwatch_conf_from_etc" do
   only_if "test -f #{SRC1}"
   user USER_NAME
-  command "install -m 644 #{SRC1} #{DST}"
+  command "install -m 644 #{SRC1} #{DST}logwatch.conf.erb"
 end
